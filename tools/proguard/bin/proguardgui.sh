703de827077b1e5e01446eb72f7a02f3a25c4d27
Li Zheng <lizheng@silan.com>
2012-04-01 10:56:17 +0800

    Init from android-sdk-r16m-windows.zip

--- tools/proguard/bin/proguardgui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Start-up script for the GUI of ProGuard -- free class file shrinker,
-# optimizer, obfuscator, and preverifier for Java bytecode.
-
-PROGUARD_HOME=`dirname "$0"`
-PROGUARD_HOME=`dirname "$PROGUARD_HOME"`
-
-java -jar $PROGUARD_HOME/lib/proguardgui.jar "$@"
